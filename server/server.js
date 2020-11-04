08af0f0e16c158cbe79b5846b4af3d1528dde729
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-11-04 00:13:09 -0700

    21.2

--- server/server.js
+++ server/server.js
@@ -9,10 +9,13 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
+const { authMiddleware } = require('./utils/auth');
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
+
 
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
